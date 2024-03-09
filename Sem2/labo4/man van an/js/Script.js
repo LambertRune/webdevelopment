b38b4992464d456c100c75a22990eb8dcdbf1ed6
lambertR <rune.lambert@student.vives.be>
2024-03-09 18:43:31 +0100

    sdaf

---
+++ Sem2/labo4/man van an/js/Script.js
@@ -0,0 +1,22 @@
+let tekst = "De man van An geeft geen hand aan ambetante verwanten";
+let zoekString = "an";
+let count = 0;
+let index = tekst.indexOf(zoekString);
+
+while (index !== -1) {
+    count++;
+    index = tekst.indexOf(zoekString, index + 1);
+}
+
+console.log("Aantal keren dat 'an' voorkomt met indexOf:", count);
+
+// Oplossing met lastIndexOf
+count = 0;
+index = tekst.lastIndexOf(zoekString);
+
+while (index !== -1) {
+    count++;
+    index = tekst.lastIndexOf(zoekString, index - 1);
+}
+
+console.log("Aantal keren dat 'an' voorkomt met lastIndexOf:", count);
