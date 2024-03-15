13bd7a74f77616178db8a904e1bdfe8ddf4718c5
lambertR <rune.lambert@student.vives.be>
2024-03-15 22:14:19 +0100

    update

--- Sem2/labo5/formwaarden/js/Script.js
+++ Sem2/labo5/formwaarden/js/Script.js
@@ -3,10 +3,28 @@ let button = document.getElementById("button");
 button.addEventListener("click",uitvoeren);
 }
 const uitvoeren = ()=>{
-     console.log("hey");
-     let isRoker = document.getElementById("roker").checked;
-     const keuzeMoedertaal = document.getElementsByTagName("moedertaal").value;
-     let buurLand = document.getElementById()
-     console.log(keuzeMoedertaal);
+     let roken = document.getElementById('roker').checked;
+     let nederlands = document.getElementById('nederlands').checked;
+     let frans = document.getElementById('frans').checked;
+     let engels = document.getElementById('engels').checked;
+     let buurland = document.getElementById('buurland').value;
+     let bestelling = document.getElementById('bestelling').options;
+     if (roken === true){
+          console.log('Rookt')
+     }
+     else{
+          console.log('Is geen roker')
+     }
+     if(nederlands === true){console.log('Moedertaal is nl')}
+     if(frans === true){console.log('Moedertaal is fr')}
+     if(engels === true){console.log('Moedertaal is en')}
+     console.log(`Favoriete buurland is ${buurland}`);
+     let bestellingZin = "bestelling bestaat uit";
+     for (let i = 0; i <bestelling.length; i++){
+          if(bestelling[i].selected === true){
+               bestellingZin += " " + bestelling[i].value;
+          }
+     }
+     console.log(bestellingZin);
 }
 window.addEventListener("load",setup);
